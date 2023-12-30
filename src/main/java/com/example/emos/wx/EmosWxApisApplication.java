ea7c1d8e9dfd6e0dfc9d815d66d97827dab9b4ce
WCeasar <285665110@qq.com>
2023-12-30 23:38:01 +0800

    swagger

---
+++ src/main/java/com/example/emos/wx/EmosWxApisApplication.java
@@ -0,0 +1,13 @@
+package com.example.emos.wx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmosWxApisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmosWxApisApplication.class, args);
+    }
+
+}
